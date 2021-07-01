5afe522d979063163a31014bf955da6675c5d5f7
RDPrangon12 <ranatdasprangon@gmail.com>
2021-07-02 01:25:33 +0600

    Footer fixed for landing page

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -6,7 +6,7 @@
 
 </script>
 
-<div style="height: {divH}px; width-auto" class="overflow-y-auto overflow-x-hidden xl:overflow-hidden">
+<div style="height: {divH}px; width-auto" class="overflow-y-auto overflow-x-hidden">
   <section class="md:ml-28 lg:mt-16 text-gray-600 body-font">
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -22,8 +22,7 @@
       </div>
     </div>
   </section>
-  <Footer /> 
-  <br><br>
+  <div class="mb-16 lg:mb-0 xl:-mt-4"><Footer /></div> 
 </div>
 
 
